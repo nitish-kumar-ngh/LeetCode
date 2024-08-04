2978b627558435bdbc625ae51fad5896e3c85e2e
Nitish Kumar Singh <154716457+nitish-kumar-ngh@users.noreply.github.com>
2024-08-04 12:54:25 +0530

    Time: 131 ms (88.11%), Space: 133.1 MB (47.65%) - LeetHub

--- 2211-k-radius-subarray-averages/2211-k-radius-subarray-averages.cpp
+++ 2211-k-radius-subarray-averages/2211-k-radius-subarray-averages.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
     vector<int> getAverages(vector<int>& nums, int k) {
-          vector<int>result;
-          int n=nums.size();
-          int i=0;
-          int j=0;
-          long long int sum=0;
-        long long   int sum1=0;
-          
-          while(j<n){
-          sum+=nums[j];
-          if (j-k<0 || j+k>=n){
-            result.push_back(-1);
-            j++;
-          }else{
-            sum1=sum;
-             for (int m=j+1;m<=j+k;m++){
-                sum1=sum1+nums[m];
-             }
+        int n= nums.size();
+        vector<int>result(n,-1);
+        if (k==0)return nums;
+        if (n<2*k+1)return result;
+        int left =0;
+        int right = 2*k;
+        int i=k;
+        long long window=0;
+        for (int i=left;i<=right;i++){
+            window+=nums[i];
+        }
+        int count=2*k+1;
+        result[i]=window/(count);
+        
+        right++;
+        while(right<n){
+            int gya=nums[left];
+            int aya=nums[right];
+            window=window-gya+aya;
+           
+            left++;
+            right++;
+            i++;
+             result[i]=window/count;
+        }
+        return result;
 
-             
-             int b=2*k+1;
-             result.push_back(sum1/b);
-             sum=sum-nums[i];
-             i++;
-             j++;
-          }
-          }
-          return result;
     }
 };
