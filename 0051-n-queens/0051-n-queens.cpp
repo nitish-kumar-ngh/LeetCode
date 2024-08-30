2860ff898daed3cc9d32952a133acb2df828aaf5
Nitish Kumar Singh <154716457+nitish-kumar-ngh@users.noreply.github.com>
2024-08-31 01:22:55 +0530

    Time: 11 ms (26.28%), Space: 11.8 MB (32.43%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,57 +1,50 @@
 class Solution {
 public:
-     vector<vector<char>>grid;
-     vector<vector<string>>result;
-     bool canSafeQueen(int row,int col,int n){
-        //row attack
-         for (int i=row-1;i>=0;i--){
-            if (grid[i][col]=='Q') return false;
-         }
-         //left diagonal check
-          for (int i=row-1, j=col-1;i>=0 and j>=0;i--,j--){
-            if (grid[i][j]=='Q') return false;
-         }
-         //right digonal check
-         for (int i=row-1,j=col+1;i>=0 and j<n;i--,j++){
-            if (grid[i][j]=='Q') return false;
-         }
-         return true;
-
-     }
-     void f(int row,int n){
-        if (row==n){
-            vector<string>temp;
-            for (int i=0;i<n;i++){
-                
-                string ans="";
-                for (int j=0;j<n;j++){
-                    ans=ans+grid[i][j];
-                }
-                temp.push_back(ans);
-            }
-            result.push_back(temp);
-            return;
-
+    
+    vector<vector<string>>result;
+    bool isSafe(int j,int i,int n,vector<vector<string>>&grid){
+        for (int k=i-1;k>=0;k--){
+            if (grid[k][j]=="Q")return false;
         }
-            
-                for (int col=0;col<n;col++){
-                    if (canSafeQueen(row,col,n)){
-                        grid[row][col]='Q';
-                        f(row+1,n);
-                        grid[row][col]='.';
-                        
-                    }
-                }
-            
-        
-
-
-     }
+        for (int k=i-1,m=j-1;k>=0 && m>=0;k--,m--){
+            if (grid[k][m]=="Q")return false;
+        }
+        for (int k=i-1,m=j+1;k>=0&&m<n;k--,m++){
+            if (grid[k][m]=="Q")return false;
+        }
+        return true;
+    }
+    void f(int n,int row,vector<vector<string>>&grid){
+      if (row>=n){
+         vector<string>temp;
+        for (int i=0;i<n;i++){
+           
+            string s="";
+            for(int j=0;j<n;j++){
+                
+                s=s+grid[i][j];
+            }
+            temp.push_back(s);
+        }
+        result.push_back(temp);
+        return;
+      }
+      for (int i=0;i<n;i++){
+       if(isSafe(i,row,n,grid)){
+           grid[row][i]="Q";
+           f(n,row+1,grid);
+           grid[row][i]=".";
+        }
+      }
+    }
     vector<vector<string>> solveNQueens(int n) {
-        grid.clear();
-        result.clear();
-        grid.resize(n,vector<char>(n,'.'));
-         f(0,n);
-         return result;
+        vector<vector<string>>grid(n,vector<string>(n,"."));
+        for (int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                cout<<grid[i][j]<<" ";
+            }cout<<"\n";
+        }
+       f(n,0,grid);
+        return result;
     }
 };
