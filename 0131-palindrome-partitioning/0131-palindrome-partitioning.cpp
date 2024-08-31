3e1d541b2c668185bd8c6c6252105b366b112262
Nitish Kumar Singh <154716457+nitish-kumar-ngh@users.noreply.github.com>
2024-08-31 18:11:15 +0530

    Time: 69 ms (91.24%), Space: 57.1 MB (72.13%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool ispalindrome(int st,int e,string &s){
+        // string k=s.substr(idx,i-idx+1);
+        // string y=k;
+        // reverse(k.begin(),k.end());
+        // if (y==k)return true;
+        // else return false;
+        while(st<=e){
+            if (s[st]==s[e]){
+                e--;
+                st++;
+            }else return false;
+        }
+        return true;
+    }
+    void f(int idx,string s,vector<string>&path, vector<vector<string>>&res){
+        if (idx==s.size()){
+            res.push_back(path);
+            return;
+
+        }
+        for (int i=idx;i<s.size();i++){
+            if (ispalindrome(idx,i,s)){
+                path.push_back(s.substr(idx,i-idx+1));
+                  f(i+1,s,path,res);
+                  path.pop_back();
+            }
+        }
+    }
+    vector<vector<string>> partition(string s) {
+       vector<vector<string>>res;
+       vector<string>path;
+       f(0,s,path,res);
+       return res;
+    }
+};
