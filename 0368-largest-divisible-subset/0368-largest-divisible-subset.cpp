9e004b37435e65095545d282627fe0e6de8596c4
Nitish Kumar Singh <154716457+nitish-kumar-ngh@users.noreply.github.com>
2025-01-10 00:34:05 +0530

    Time: 43 ms (7.1%), Space: 12.7 MB (20.03%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
@@ -1,38 +1,40 @@
 class Solution {
 public:
-   vector<int> largestDivisibleSubset(vector<int>& nums) {
-    int n = nums.size();
-    vector<int> v(n, 1);
-    vector<int> p(n, 0);
-    int last = 0;
-    int maxi = 1;
-
-    
-    sort(nums.begin(), nums.end());
-
-    for (int i = 0; i < n; i++) {
-        p[i] = i;
-        for (int j = 0; j < i; j++) {
-            if ((nums[i] % nums[j] == 0) && v[j] + 1 > v[i]) {
-                v[i] = v[j] + 1;
-                p[i] = j;
+    vector<int> largestDivisibleSubset(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>v(n,1);
+         vector<int>p(n,0);
+         int last=0;
+         int maxi=1;
+         sort(nums.begin(),nums.end());
+        for(int i=0;i<n;i++){
+            p[i]=i;
+            for(int j=0;j<i;j++){
+                if((nums[i]%nums[j]==0||nums[j]%nums[i]==0)&&v[j]+1>v[i]){
+                  //  v[i]=max(v[j]+1,v[i]);
+                  v[i]=v[j]+1;
+                  p[i]=j;
+                }
+                if (v[i]>maxi){
+                    maxi=v[i];
+                    last=i;
+                }
             }
         }
-        if (v[i] > maxi) {
-            maxi = v[i];
-            last = i;
-        }
-    }
-
-    vector<int> ans;
-    while (p[last] != last) {
-        ans.push_back(nums[last]);
-        last = p[last];
-    }
-    ans.push_back(nums[last]); 
-    reverse(ans.begin(), ans.end());
-    return ans;  
-
-
+        cout<<last<<" ";
+         for(int i=0;i<n;i++){
+            cout<<v[i]<<" ";
+         }
+         vector<int>ans;
+       
+         ans.push_back(nums[last]);
+         while(p[last]!=last){
+            int a=nums[p[last]];
+             ans.push_back(a);
+            last= p[last];
+         }
+         reverse(ans.begin(),ans.end());
+        
+         return ans;
     }
 };
