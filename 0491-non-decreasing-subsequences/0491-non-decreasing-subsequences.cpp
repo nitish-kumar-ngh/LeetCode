c37aa0ff5dfead5c63dc26251570ac38a801bd98
Nitish Kumar Singh <154716457+nitish-kumar-ngh@users.noreply.github.com>
2025-01-12 19:58:49 +0530

    Time: 152 ms (13%), Space: 104.2 MB (5.76%) - LeetHub

--- 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
@@ -2,41 +2,28 @@ class Solution {
 public:
    set<vector<int>>s;
    int n;
-   void f(int idx,vector<int>& nums,vector<int>&temp){
-   // if (idx>=n)return;
-    if(temp.size()>=2){
+   void f(int idx,vector<int>& nums,vector<int>temp){
+    if (idx>=n){
+         if(temp.size()>=2){
         s.insert(temp);
     }
-    for(int i=idx;i<n;i++){
-  if (temp.empty()||nums[i]>=temp.back()){
-        temp.push_back(nums[i]);
-        f(i+1,nums,temp);
+    return;
+    }
+    
+    if (temp.empty()||nums[idx]>=temp.back()){
+        temp.push_back(nums[idx]);
+        f(idx+1,nums,temp);
         temp.pop_back();
-     
+        f(idx+1,nums,temp);
+    }else{
+        f(idx+1,nums,temp);
     }
-    
-    }
-    
    }
-
-    // void f(int idx, vector<int>& nums, vector<int>& temp) {
-    //     if (temp.size() >= 2) {
-    //         s.insert(temp);
-    //     }
-
-    //     for (int i = idx; i < n; i++) {
-    //         if (temp.empty() || nums[i] >= temp.back()) {
-    //             temp.push_back(nums[i]);
-    //             f(i + 1, nums, temp);
-    //             temp.pop_back();
-    //         }
-    //     }
-    // }
     vector<vector<int>> findSubsequences(vector<int>& nums) {
         s.clear();
        n=nums.size();
        vector<int>temp;
-       f(0,nums,temp);
+       f(0,nums,{});
        vector<vector<int>> result;
        for(auto &v:s){
         result.push_back(v);
