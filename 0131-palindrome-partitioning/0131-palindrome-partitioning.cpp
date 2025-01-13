06d2d645c79be2061eb76ea19006a9cfe2cfe819
Nitish Kumar Singh <154716457+nitish-kumar-ngh@users.noreply.github.com>
2025-01-13 12:25:34 +0530

    Time: 217 ms (5.94%), Space: 199.1 MB (5%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,37 +1,27 @@
 class Solution {
 public:
-    bool ispalindrome(int st,int e,string &s){
-        string k=s.substr(st,e-st+1);
-        string y=k;
-        reverse(k.begin(),k.end());
-        if (y==k)return true;
-        else return false;
-        // while(st<=e){
-        //     if (s[st]==s[e]){
-        //         e--;
-        //         st++;
-        //     }else return false;
-        // }
-        // return true;
-    }
-    void f(int idx,string s,vector<string>&path, vector<vector<string>>&res){
-        if (idx==s.size()){
-            res.push_back(path);
-            return;
-
+ vector<vector<string>>result;
+  bool ispalindrom(string s){
+    string a=s;
+    reverse(a.begin(),a.end());
+    if(a==s)return true;
+    else return false;
+  }
+   void f(int idx,string s,vector<string>temp){
+    if (idx==s.size()){
+        result.push_back(temp);
+        return;
         }
-        for (int i=idx;i<s.size();i++){
-            if (ispalindrome(idx,i,s)){
-                path.push_back(s.substr(idx,i-idx+1));
-                  f(i+1,s,path,res);
-                  path.pop_back();
-            }
+    for(int i=idx;i<=s.size();i++){
+        if (ispalindrom(s.substr(idx,i-idx+1))){
+            temp.push_back(s.substr(idx,i-idx+1));
+            f(i+1,s,temp);
+            temp.pop_back();
         }
     }
+   }
     vector<vector<string>> partition(string s) {
-       vector<vector<string>>res;
-       vector<string>path;
-       f(0,s,path,res);
-       return res;
+    f(0,s,{});
+    return result;
     }
 };
