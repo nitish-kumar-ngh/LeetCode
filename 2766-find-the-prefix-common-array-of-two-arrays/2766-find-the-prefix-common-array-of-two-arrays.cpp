0b9f5c1216493671d5f2184043ede07ef1e04f97
Nitish Kumar Singh <154716457+nitish-kumar-ngh@users.noreply.github.com>
2025-01-14 19:47:23 +0530

    Time: 6 ms (60.48%), Space: 88.4 MB (31.97%) - LeetHub

--- 2766-find-the-prefix-common-array-of-two-arrays/2766-find-the-prefix-common-array-of-two-arrays.cpp
+++ 2766-find-the-prefix-common-array-of-two-arrays/2766-find-the-prefix-common-array-of-two-arrays.cpp
@@ -4,19 +4,16 @@ public:
         int n=A.size();
          vector<int> ans;
          int cnt=0;
-        for(int i=0;i<n;i++){
-            cnt=0;
-            for(int j=0;j<=i;j++){
-                for(int k=0;k<=i;k++){
-                    if(A[j]==B[k]){
-                        cnt++;
-                        break;
-                    }
-                }
-              
-            }
-              ans.push_back(cnt);
-        }
+         unordered_map<int ,int>mp;
+         for(int i=0;i<A.size();i++){
+            mp[A[i]]++;
+            if (mp[A[i]]==2)cnt+=1;
+            mp[B[i]]++;
+            if (mp[B[i]]==2)cnt+=1;
+            ans.push_back(cnt);
+         }
+         
+        
         return ans;
     }
 };
