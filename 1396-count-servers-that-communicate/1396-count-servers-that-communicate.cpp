a678de0a50cdb7fe00e08dfd0a648a8b09e7197f
Nitish Kumar Singh <154716457+nitish-kumar-ngh@users.noreply.github.com>
2025-01-23 23:49:01 +0530

    Time: 0 ms (100%), Space: 26.1 MB (89.76%) - LeetHub

---
+++ 1396-count-servers-that-communicate/1396-count-servers-that-communicate.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int countServers(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        int resultServers = 0;
+
+        // Traverse through the grid
+        for (int row = 0; row < m; ++row) {
+            for (int col = 0; col < n; ++col) {
+                if (grid[row][col] == 1) {
+                    bool canCommunicate = false;
+
+                    // Check for communication in the same row
+                    for (int otherCol = 0; otherCol < n; ++otherCol) {
+                        if (otherCol != col && grid[row][otherCol] == 1) {
+                            canCommunicate = true;
+                            break;
+                        }
+                    }
+
+                    // If a server was found in the same row, increment resultServers
+                    if (canCommunicate) {
+                        resultServers++;
+                    } else {
+                        // Check for communication in the same column
+                        for (int otherRow = 0; otherRow < m; ++otherRow) {
+                            if (otherRow != row && grid[otherRow][col] == 1) {
+                                canCommunicate = true;
+                                break;
+                            }
+                        }
+
+                        // If a server was found in the same column, increment resultServers
+                        if (canCommunicate) {
+                            resultServers++;
+                        }
+                    }
+                }
+            }
+        }
+
+        return resultServers;
+    }
+};
