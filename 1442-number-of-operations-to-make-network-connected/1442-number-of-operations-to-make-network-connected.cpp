19a756708541630edd61392c1ab609a39423ba6e
Nitish Kumar Singh <154716457+nitish-kumar-ngh@users.noreply.github.com>
2025-01-31 08:41:23 +0530

    Time: 24 ms (42.04%), Space: 46.5 MB (66.32%) - LeetHub

--- 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.cpp
+++ 1442-number-of-operations-to-make-network-connected/1442-number-of-operations-to-make-network-connected.cpp
@@ -1,41 +1,56 @@
+class DisjointSet{
+public:
+vector<int>rank;
+vector<int>parent;
+DisjointSet(int n){
+
+rank.resize(n,0);
+parent.resize(n,0);
+for (int i=0;i<n;i++){
+    parent[i]=i;
+}}
+int find_parent(int x){
+    if (x==parent[x])return x;
+    return parent[x]=find_parent(parent[x]);
+}
+void union_by_rank(int u,int v){
+    int ulp_u=find_parent(u);
+     int ulp_v=find_parent(v);
+     if (ulp_u==ulp_v)return;
+     if (rank[ulp_u]>rank[ulp_v]){
+        parent[ulp_v]=ulp_u;
+     }else if (rank[ulp_u]<rank[ulp_v]){
+           parent[ulp_u]=ulp_v;
+     }else {
+           parent[ulp_u]=ulp_v;;
+           rank[ulp_v]++;
+     }
+}
+
+};
+
+
+
+
 class Solution {
 public:
-    vector<int>parent;
-    vector<int>rank;
-    int find(int x){
-        if (parent[x]==x)return x;
-        return parent[x]=find(parent[x]);
-    }
-    void Union(int x,int y){
-        int x_parent=find(x);
-        int y_parent=find(y);
-        if (x_parent == y_parent)return ;
-        if (rank[x_parent]>rank[y_parent]){
-            parent[y_parent]=x_parent;
-        }else if (rank[x_parent]<rank[y_parent]){
-            parent[x_parent]=y_parent;
-        }else{
-            parent[x_parent]=y_parent;
-            y_parent++;
-        }
-    }
     int makeConnected(int n, vector<vector<int>>& connections) {
-        parent.resize(n);
-        for (int i=0;i<n;i++){
-            parent[i]=i;
-        }
-        rank.resize(n,0);
-        int noOfEdges=connections.size();
-        int components=n;
-        if (noOfEdges<n-1)return -1;
-        for (auto x:connections){
-            int a=x[0];
-            int b=x[1];
-            if (find(a)!=find(b)){
-                Union(a,b);
-                components--;
+        DisjointSet ds(n);
+        int cnt=0,connecop=0;
+        for (auto v:connections){
+            int x=v[0];
+            int y=v[1];
+            if (ds.find_parent(x)==ds.find_parent(y))cnt++;
+            else{
+                ds.union_by_rank(x,y);
             }
         }
-        return components-1;
+
+        for (int i=0;i<n;i++){
+            if (ds.parent[i]==i)connecop++;
+        }
+        int ans=connecop-1;
+        if (cnt>=ans)return ans;
+        else return -1;
     }
 };
