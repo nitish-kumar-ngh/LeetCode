d3f8efe0e3d4741aa8f230b80be47bb9e624198b
Nitish Kumar Singh <154716457+nitish-kumar-ngh@users.noreply.github.com>
2025-02-17 18:50:48 +0530

    Time: 95 ms (34.64%), Space: 56.1 MB (63.54%) - LeetHub

--- 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.cpp
+++ 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.cpp
@@ -1,34 +1,23 @@
 class Solution {
 public:
- int f(vector<int>& nums, int k){
-    int r=0,l=0,cnt=0;
-    unordered_map<int,int>mp;
-    while(r<nums.size()){
-        mp[nums[r]]++;
-        while(mp.size()>k){
+    int f(vector<int>& nums,int k){
+        int ans=0;
+        int l=0;
+        int r=0;
+        unordered_map<int,int>mp;
+        while(r<nums.size()){
+          mp[nums[r]]++;
+          while(mp.size()>k){
             mp[nums[l]]--;
-            if (mp[nums[l]]==0)mp.erase(nums[l]);
-            l=l+1;
+            if(mp[nums[l]]==0)mp.erase(nums[l]);
+            l++;
+          }
+       ans+=r-l+1;
+          r++;
         }
-        cnt=cnt + (r-l+1);
-        r++;
-       
+        return ans;
     }
-     return cnt;
- }
     int subarraysWithKDistinct(vector<int>& nums, int k) {
-        // int cnt=0;
-        // for(int i=0;i<nums.size();i++){
-        //     unordered_set<int>st;
-        //     for(int j=i;j<nums.size();j++){
-        //         st.insert(nums[j]);
-        //         if (st.size()==k)cnt++;
-        //         else if (st.size()>k)break;
-        //     }
-        // }
-        // return cnt;
-        int a=f(nums,k);
-        int b=f(nums,k-1);
-        return abs(a-b);
+        return f(nums,k)-f(nums,k-1);
     }
 };
