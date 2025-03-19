3989fd55374eb16a9920b920f907422b92ffedd5
Nitish Kumar Singh <154716457+nitish-kumar-ngh@users.noreply.github.com>
2025-03-20 00:47:16 +0530

    Time: 2667 ms (5.03%), Space: 489.9 MB (5.03%) - LeetHub

--- 3570-count-of-substrings-containing-every-vowel-and-k-consonants-i/3570-count-of-substrings-containing-every-vowel-and-k-consonants-i.cpp
+++ 3570-count-of-substrings-containing-every-vowel-and-k-consonants-i/3570-count-of-substrings-containing-every-vowel-and-k-consonants-i.cpp
@@ -1,32 +1,33 @@
 class Solution {
 public:
-    bool isVowel(char c) {
-    return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
-}
-
-int countOfSubstrings(string word, int l) {
-    int ans = 0, n = word.size();
-
-    for (int i = 0; i < n; i++) {
-        unordered_set<char> vowels;
-        int nonVowelCount = 0;
-
-        for (int j = i; j < n; j++) {
-            char c = word[j];
-
-            if (isVowel(c)) 
-                vowels.insert(c); 
-            else 
-                nonVowelCount++;
-
-            int len = j - i + 1;
-            if (len >= l + 5 && vowels.size() == 5 && nonVowelCount == l) {
-                ans++;
+    int countOfSubstrings(string word, int l) {
+        int ans=0;
+        for (int i=0;i<word.size();i++){
+            
+            for(int j=i;j<word.size();j++){
+                if (j-i+1>=l+5){
+                    unordered_map<char,int>ch;
+                    for(int k=i;k<=j;k++){
+                    ch[word[k]]++;
+                    }
+                    int cnt =0,cnt2=0;
+                   if (ch.find('a')!=ch.end() && ch.find('e')!=ch.end() && ch.find('i')!=ch.end()&&ch.find('o')!=ch.end()&&ch.find('u')!=ch.end()){
+                    for(auto v:ch){
+                        if (v.first !='a'&&v.first !='e'&&v.first !='i'&&v.first !='o'&&v.first !='u'){
+                            cnt+=v.second;
+                           
+                            }else{
+                                cnt2++;
+                            }
+                    }
+                   
+                   }
+                    if (cnt==l &&cnt2==5){
+                         cout<<"("<<i<<j<<") ";
+                        ans++;}
+                }
             }
         }
+        return ans;
     }
-
-    return ans;
-}
-    
 };
