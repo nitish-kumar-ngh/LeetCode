a8f7efe4997582c543b19640cde8620968310e5c
Nitish Kumar Singh <154716457+nitish-kumar-ngh@users.noreply.github.com>
2025-03-22 12:39:30 +0530

    Time: 33 ms (26.24%), Space: 24.4 MB (18.54%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -3,14 +3,24 @@ public:
   int dx[4]={1,0,-1,0};
   int dy[4]={0,1,0,-1};
   int n,m;
-  void dfs (int i,int j,vector<vector<char>>& grid,vector<vector<int>>&vis){
-    if (i>=n || j>=m ||i<0 ||j<0||grid[i][j]=='0'||vis[i][j]==1)return;
-     vis[i][j]=1;
+  void bfs (int i,int j,vector<vector<char>>& grid,vector<vector<int>>&vis){
+    queue<pair<int,int>>q;
+    q.push({i,j});
+    vis[i][j]=1;
+    while(q.size()>0){
+    auto v=q.front();
+    q.pop();
+
      for (int d=0;d<4;d++){
-        int i_=i+dx[d];
-        int j_=j+dy[d];
-        dfs(i_,j_,grid,vis);
+        int i_=v.first+dx[d];
+        int j_=v.second+dy[d];
+        if (i_>=0 &&j_>=0&&i_<n&&j_<m&&vis[i_][j_]==0&&grid[i_][j_]=='1'){
+            q.push({i_,j_});
+            vis[i_][j_]=1;
+        }
+        
      }
+  }
 
      
   }
@@ -22,7 +32,7 @@ public:
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if (grid[i][j]=='1'&& !vis[i][j]){
-                    dfs(i,j,grid,vis);
+                    bfs(i,j,grid,vis);
                     cnt++;
                 }
             }
