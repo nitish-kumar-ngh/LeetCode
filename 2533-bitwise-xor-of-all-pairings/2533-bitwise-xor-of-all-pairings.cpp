8b214dc7a4a2f05eaecb47f80e2f68e33539ff7f
Nitish Kumar Singh <154716457+nitish-kumar-ngh@users.noreply.github.com>
2025-07-26 11:03:05 +0530

    Time: 0 ms (100%), Space: 64.4 MB (14.32%) - LeetHub

--- 2533-bitwise-xor-of-all-pairings/2533-bitwise-xor-of-all-pairings.cpp
+++ 2533-bitwise-xor-of-all-pairings/2533-bitwise-xor-of-all-pairings.cpp
@@ -1,33 +1,35 @@
 class Solution {
 public:
     int xorAllNums(vector<int>& nums1, vector<int>& nums2) {
-        int a=nums1.size();
-        int b=nums2.size();
-        int k=0;
-        cout<<a<<" "<<b;
-        if (a%2==0){
-            if (b%2==0){//odd
-                   k=0;
-            }else{
-                for(int i=0;i<a;i++){
-                    k=k^nums1[i];
-                }
-            }
-            
-        }else{
-            if (b%2==0){
-                 for(int i=0;i<b;i++){
-                    k=k^nums2[i];
-                }
-            }else{
-                for(int i=0;i<a;i++){
-                    k=k^nums1[i];
-                }
-                for(int i=0;i<b;i++){
-                    k=k^nums2[i];
-                }
-            }
+        
+        int ans =0;
+        // for(int i=0;i<nums1.size();i++){
+        //     for(int j=0;j<nums2.size();j++){
+        //         int a= nums1[i]^nums2[j];
+              
+        //         ans=ans^a;
+        //     }
+        // }
+        // return ans;
+        int n = nums1.size();
+        int m=nums2.size();
+        if(n%2!=0 && m%2!=0){//odd
+         for(int i=0;i<nums1.size();i++){
+            ans= ans^nums1[i];
+         }
+           for(int i=0;i<nums2.size();i++){
+            ans =ans^nums2[i];
+         }
+
+        }else if (n%2!=0 && m%2==0){
+            for(int i=0;i<nums2.size();i++){
+            ans =ans^nums2[i];
+         }
+        }else if (n%2==0 && m%2!=0){
+            for(int i=0;i<nums1.size();i++){
+            ans= ans^nums1[i];
+         }
         }
-        return k;
+        return ans;
     }
 };
