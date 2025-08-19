7ad7772ecb15b9c52f6b28aa4a0c0c2391a3b7bd
Nitish Kumar Singh <154716457+nitish-kumar-ngh@users.noreply.github.com>
2025-08-19 21:53:18 +0530

    Time: 49 ms (84.01%), Space: 13.1 MB (83.06%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
-    int t1[1001][1001];
-
-    int f(string &s, string &t, int i1, int i2) {
-        if(i1 >= s.size() || i2 >= t.size()) return 0;
-
-        if(t1[i1][i2] != -1) return t1[i1][i2];
-
-        if(s[i1] == t[i2]) {
-            return t1[i1][i2] = 1 + f(s, t, i1+1, i2+1);
-        } else {
-            return t1[i1][i2] = max(f(s, t, i1+1, i2),
-                                    f(s, t, i1, i2+1));
+   int t1[1001][1001];
+     int f(string &s,string &t,int i1,int i2){
+        if(i1>=s.size() || i2>=s.size())return t1[i1][i2]= 0;
+        int ans=INT_MIN;
+        if( t1[i1][i2]!=-1)return  t1[i1][i2];
+        if(s[i1]==t[i2]){
+     // ans+=1+f(s,t,i1+1,i2+1);
+     ans=max(ans,1+f(s,t,i1+1,i2+1));
+        }else{
+           // ans=ans+max(f(s,t,i1+1,i2),f(s,t,i1,i2+1));
+           ans=max(ans,max(f(s,t,i1+1,i2),f(s,t,i1,i2+1)));
         }
-    }
-
+        return  t1[i1][i2]=ans;
+     }
     int longestPalindromeSubseq(string s) {
-        memset(t1, -1, sizeof(t1));
-        string t = s;
-        reverse(t.begin(), t.end());
-        return f(s, t, 0, 0);
+        memset(t1,-1,sizeof(t1));
+        string t=s;
+         reverse(t.begin(),t.end());
+        return f(s,t,0,0);
     }
 };
