913ecc17f808c8756d77d6336d36484009098c8d
Nitish Kumar Singh <154716457+nitish-kumar-ngh@users.noreply.github.com>
2025-08-23 19:09:48 +0530

    Time: 175 ms (59.94%), Space: 120 MB (65.66%) - LeetHub

--- 2022-maximum-alternating-subsequence-sum/2022-maximum-alternating-subsequence-sum.cpp
+++ 2022-maximum-alternating-subsequence-sum/2022-maximum-alternating-subsequence-sum.cpp
@@ -1,31 +1,24 @@
 class Solution {
 public:
-    int n;
-    long long t[1000001][2];
-    long long solve(int idx, vector<int>& nums, bool iseven) {
-        
-        if(idx >= n) {
-            return 0;
+  long long t[1000001][2];
+  int n;
+  long  long f(vector<int>& nums,int idx,int prev){
+        if(idx>=n)return 0;
+        if(t[idx][prev]!=-1)return t[idx][prev];
+       long long  skip = f(nums,idx+1,prev);
+      long long  val =nums[idx];
+        if(prev == 0){
+             val=-nums[idx];
         }
-        if(t[idx][iseven] != -1)
-            return t[idx][iseven];
-        
-        long long skip = solve(idx+1, nums, iseven);
-        
-        long long val = nums[idx];
-        if(iseven == false) {
-            val = -val;
-        }
-        
-        long long take = solve(idx+1, nums, !iseven) + val;
-        
-        return t[idx][iseven] = max(skip, take);
-        
+        long long  take = val+f(nums,idx+1,!prev);
+
+        return t[idx][prev]= max(take,skip);
     }
-    
     long long maxAlternatingSum(vector<int>& nums) {
-        n = nums.size();
-        memset(t, -1, sizeof(t));
-        return solve(0, nums, true);
+        n=nums.size();
+         memset(t,-1,sizeof(t));
+         return f(nums,0,1);
+
+
     }
 };
