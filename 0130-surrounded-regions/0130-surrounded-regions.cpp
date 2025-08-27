93cde60fffc555ac5bf96eb13432330bb8aec6c9
Nitish Kumar Singh <154716457+nitish-kumar-ngh@users.noreply.github.com>
2025-08-27 17:47:57 +0530

    Time: 0 ms (100%), Space: 15 MB (27.42%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,43 +1,58 @@
 class Solution {
 public:
     void solve(vector<vector<char>>& board) {
-        int m=board.size();
-        int n=board[0].size();
-        vector<vector<int>>vis(m,vector<int>(n,0));
+        int r = board.size();
+        int c = board[0].size();
+        vector<vector<int>>vis(r,vector<int>(c,0));
         queue<pair<int,int>>q;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if (i==0 || j==0 ||i==m-1||j==n-1){
-                    if (board[i][j]=='O'){
-                        q.push({i,j});
-                        vis[i][j]=1;
-                    }
+        for(int i=0;i<r;i++){
+            if(board[i][0]=='O'){
+                q.push({i,0});
+                vis[i][0]=1;
                 }
+        }
+         for(int i=0;i<r;i++){
+            if(board[i][c-1]=='O'){
+                q.push({i,c-1});
+                vis[i][c-1]=0;
             }
         }
-        int dx[]={0,1,0,-1};
-        int dy[]={1,0,-1,0};
-        while(q.size()>0){
-            auto v=q.front();
-            q.pop();
-            int x=v.first;
-            int y=v.second;
-            for(int i=0;i<4;i++){
-                int newx=x+dx[i];
-                int newy=y+dy[i];
-                if (newx>=0&&newy>=0&&newx<m&&newy<n&&vis[newx][newy]==0&&board[newx][newy]=='O'){
-                    q.push({newx,newy});
-                    vis[newx][newy]=1;
+
+         for(int i=0;i<c;i++){
+            if(board[0][i]=='O'){
+                q.push({0,i});
+                vis[0][i]=1;
                 }
+        }
+         for(int i=0;i<c;i++){
+            if(board[r-1][i]=='O'){
+                q.push({r-1,i});
+                vis[r-1][i]=1;
+                }
+        }
+      int dx[]={-1,1,0,0};
+      int dy[]={0,0,-1,1};
+      while(q.size()>0){
+        int x = q.front().first;
+        int y = q.front().second;
+        q.pop();
+        for(int d =0;d<4;d++){
+            int x_ = x+dx[d];
+            int y_ = y+dy[d];
+
+            if(x_>=0 && x_<r && y_>=0 && y_<c && board[x_][y_]=='O' && vis[x_][y_]==0){
+                q.push({x_,y_});
+                vis[x_][y_]=1;
             }
         }
-          for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                    if (board[i][j]=='O' &&vis[i][j]==0){
-                       board[i][j]='X';
-                    }
-                }
+
+      }
+      for(int i=0;i<r;i++){
+        for(int j =0;j<c;j++){
+            if(board[i][j]=='O'&& vis[i][j]==0 && i!=0 && i!=r-1 && j!=0 && j!=c-1){
+                board[i][j]='X';
             }
-       // return board;
+        }
+      }
     }
 };
