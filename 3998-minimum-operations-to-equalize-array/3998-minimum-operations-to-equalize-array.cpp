38231cc773d64157af4819dac2c6d35f9d93f3cf
Nitish Kumar Singh <154716457+nitish-kumar-ngh@users.noreply.github.com>
2025-09-07 12:49:02 +0530

    Time: 0 ms (100%), Space: 31.6 MB (8.33%) - LeetHub

---
+++ 3998-minimum-operations-to-equalize-array/3998-minimum-operations-to-equalize-array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        set<int>s;
+        for(int i=0;i<nums.size();i++){
+            s.insert(nums[i]);
+        }
+        if(s.size()==1)return 0;
+        else return 1;
+    }
+};
