18560526e453e14f4c4d248fe4cefed6a1320cf8
Nitish Kumar Singh <154716457+nitish-kumar-ngh@users.noreply.github.com>
2025-09-12 11:01:41 +0530

    Time: 4 ms (78.54%), Space: 18.9 MB (95.71%) - LeetHub

--- 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.cpp
+++ 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     bool doesAliceWin(string s) {
-        int cnt=0;
+          int cnt=0;
         for (int i=0;i<s.size();i++){
             if (s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u')cnt++;
         }
         if (cnt==0)return false;
         else return true;
+    
     }
 };
