e650acd69de5177d2967b45722e6d5e56756fac9
Nitish Kumar Singh <154716457+nitish-kumar-ngh@users.noreply.github.com>
2025-09-26 14:21:31 +0530

    Time: 0 ms (100%), Space: 10.7 MB (6.12%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string ans="";
+        for(int i =0;i<s.size();i++){
+            if(s[i]>=65 && s[i]<=90){
+              ans+=(s[i]+32);
+            }else if(s[i]>=97 && s[i]<=122){
+                ans+=(s[i]);
+            }else if(s[i]>=48 && s[i]<=57)ans+=(s[i]);
+        }
+        cout<<ans;
+        string k=ans;
+        reverse(ans.begin(),ans.end());
+
+        return k==ans;
+    }
+};
