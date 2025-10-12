fea907a9f092884277233ee19d750aec9898c901
Nitish Kumar Singh <154716457+nitish-kumar-ngh@users.noreply.github.com>
2025-10-12 13:20:02 +0530

    Time: 147 ms (6.47%), Space: 15.8 MB (95.19%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
-int ans=INT_MAX;
-int t[13][10004];
-    int f(vector<int>& coins,int i,int amt){
-        if (amt==0)return 0;
-        if (amt<0)return 1e8;
-        if (i>=coins.size())return 1e8;
-        if (t[i][amt]!=-1)return t[i][amt];
-        int ans=1e8;
-        if(coins[i]<=amt){
-           ans=min(ans,1+f(coins,i,amt-coins[i]));
-               ans=min(ans,1+f(coins,i+1,amt-coins[i]));
-             ans=min(ans,f(coins,i+1,amt));
-        }else{
-             ans=min(ans,f(coins,i+1,amt));
+    int n;
+    int t[13][10004];
+    int f(int idx,vector<int>& coins,int amount){
+        if(amount == 0) return 0;
+        if(idx>=n)return 1e9;
+        if(t[idx][amount]!=-1)return t[idx][amount];
+        int ans = INT_MAX;
+        if(coins[idx]<=amount){
+        ans = min({ans,1+f(idx,coins,amount-coins[idx]),f(idx+1,coins,amount)});
         }
-        return t[i][amt]= ans;
+        ans = min(ans,f(idx+1,coins,amount));
+        return t[idx][amount]= ans;
     }
     int coinChange(vector<int>& coins, int amount) {
+        n=coins.size();
         memset(t,-1,sizeof(t));
-       return f(coins,0,amount)==1e8?-1:f(coins,0,amount);
-       // return ans==INT_MAX?-1:ans;
+       return f(0,coins,amount)>=1e9 ?-1:f(0,coins,amount);
     }
 };
