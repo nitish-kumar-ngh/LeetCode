e072b4f4296c9bc09965a1be2b5589bdb7c0cf65
Nitish Kumar Singh <154716457+nitish-kumar-ngh@users.noreply.github.com>
2025-10-13 18:34:17 +0530

    Time: 64 ms (25.63%), Space: 81 MB (20.64%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
-    vector<vector<string>>ans;
-    bool p(string t){
-        string k=t;
-        reverse(t.begin(),t.end());
-        return k==t;
-    }
-    void f(string s,int idx,vector<string>&temp){
-        if(idx==s.size()){
+     vector<vector<string>>ans;
+     bool p(string s){
+        string t=s;
+        reverse(s.begin(),s.end());
+        return s==t;
+     }
+    void f(int idx,string &s,vector<string>&temp){
+        if(idx>=s.size()){
             ans.push_back(temp);
-            return ;
+            return;
         }
+
         for(int i=idx;i<s.size();i++){
-            string t = s.substr(idx,i-idx+1);
+            string t= s.substr(idx,i-idx+1);
             if(p(t)){
                 temp.push_back(t);
-                f(s,i+1,temp);
+                f(i+1,s,temp);
                 temp.pop_back();
             }
         }
     }
     vector<vector<string>> partition(string s) {
         vector<string>temp;
-         f(s,0,temp);
-         return ans;
+        f(0,s,temp);
+        return ans;
     }
 };
