a12abea8abbe8f66aa35a81ef65cde69ff4fb7d8
Nitish Kumar Singh <154716457+nitish-kumar-ngh@users.noreply.github.com>
2025-10-14 18:30:25 +0530

    Time: 0 ms (100%), Space: 9 MB (29.53%) - LeetHub

--- 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.cpp
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
     int minMutation(string startGene, string endGene, vector<string>& bank) {
+        string b = "ACGT";
+        unordered_set<string>st(bank.begin(),bank.end());
         queue<pair<string,int>>q;
-        set<string>s(bank.begin(),bank.end());
         q.push({startGene,0});
-        string k="ACGT";
         while(q.size()>0){
-            auto v=q.front();
-            string ans=v.first;
-            int d=v.second;
-            if (ans==endGene)return d;
+            string a=q.front().first;
+            int d = q.front().second;
+            if(a==endGene){
+                return d;
+            }
             q.pop();
-            for (int i=0;i<ans.size();i++){
-                char original=ans[i];
-                for (int j=0;j<k.size();j++){
-                    ans[i]=k[j];
-                    if (s.find(ans)!=s.end()){
-                        q.push({ans,d+1});
-                        s.erase(ans);
+            for(int i=0;i<a.size();i++){
+                char original = a[i];
+                for(int j=0;j<b.size();j++){
+                    a[i]=b[j];
+                    if(st.find(a)!=st.end()){
+                        st.erase(a);
+                    q.push({a,d+1});
                     }
                 }
-                ans[i]=original;
-
+                a[i]=original;
             }
         }
-        return -1;
+
+       return -1;
+
     }
 };
