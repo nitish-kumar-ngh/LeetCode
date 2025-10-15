18c28e8a25610de8d541aad0e2f7d98496bf89d3
Nitish Kumar Singh <154716457+nitish-kumar-ngh@users.noreply.github.com>
2025-10-15 09:51:10 +0530

    Time: 2 ms (89.87%), Space: 39.4 MB (34.66%) - LeetHub

--- 2038-nearest-exit-from-entrance-in-maze/2038-nearest-exit-from-entrance-in-maze.cpp
+++ 2038-nearest-exit-from-entrance-in-maze/2038-nearest-exit-from-entrance-in-maze.cpp
@@ -1,40 +1,33 @@
 class Solution {
 public:
-    typedef pair<int, pair<int, int>> p;
+    
     int nearestExit(vector<vector<char>>& maze, vector<int>& entrance) {
-        int m=maze.size();
-        int n=maze[0].size();
-        vector<vector<int>>vis(m,vector<int>(n,0));
-        priority_queue<p, vector<p>, greater<p>> q;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if (i==0 || i==m-1 ||j==0 || j==n-1){
-                if (maze[i][j]=='.'){
-                    if (i==entrance[0]&&j==entrance[1])continue;
-                    q.push({0,{i,j}});
-                    vis[i][j]=1;
-                }}
+    queue<pair<int,pair<int,int>>>q;
+    q.push({0,{entrance[0],entrance[1]}});
+    int r = maze.size();
+    int c = maze[0].size();
+    vector<vector<int>>vis(r,vector<int>(c,0));
+    vis[entrance[0]][entrance[1]]=1;
+    int dx[]={0,0,1,-1};
+    int dy[]={1,-1,0,0};
+    while(q.size()>0){
+
+        int d = q.front().first;
+        int x = q.front().second.first;
+        int y = q.front().second.second;
+        q.pop();
+        if((x!=entrance[0] || y!=entrance[1]) && (x==r-1||x==0||y==c-1||y==0 )  )return d;
+
+        for(int i=0;i<4;i++){
+            int newx = x+dx[i];
+            int newy = y+dy[i];
+            if(newx>=0 && newx<r && newy>=0&& newy<c && vis[newx][newy]==0 && maze[newx][newy]=='.'){
+                q.push({d+1,{newx,newy}});
+                vis[newx][newy]=1;
             }
         }
-        int dx[]={1,0,-1,0};
-        int dy[]={0,1,0,-1};
-        while(q.size()>0){
-            auto a=q.top();
-            int d=a.first;
-            int x=a.second.first;
-            int y=a.second.second;
-            q.pop();
-            if (x==entrance[0] && y==entrance[1])return d;
-            for (int j=0;j<4;j++){
-                int new_x=x+dx[j];
-                int new_y=y+dy[j];
-                if (new_x>=0 && new_y>=0 && new_x<m && new_y<n &&maze[new_x][new_y]=='.' && vis[new_x][new_y]==0){
-                    q.push({d+1,{new_x,new_y}});
-                    vis[new_x][new_y]=1;
-                }
-            }
-   
-        }
-        return -1;
+
+    }    
+    return -1;
     }
 };
