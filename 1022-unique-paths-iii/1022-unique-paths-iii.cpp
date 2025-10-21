4f50eacd9eba141fefaef2a529b431b1cd0059bf
Nitish Kumar Singh <154716457+nitish-kumar-ngh@users.noreply.github.com>
2025-10-21 13:29:27 +0530

    Time: 0 ms (100%), Space: 9.3 MB (76.49%) - LeetHub

--- 1022-unique-paths-iii/1022-unique-paths-iii.cpp
+++ 1022-unique-paths-iii/1022-unique-paths-iii.cpp
@@ -1,41 +1,66 @@
 class Solution {
 public:
-   int result=0,noObobstacles=0;
-   int m,n;
-   vector<vector<int>>dir={{1,0},{-1,0},{0,1},{0,-1}};
-   void f(int i,int j,int count,vector<vector<int>>& grid){
-    if (i<0||i>=m||j<0||j>=n||grid[i][j]==-1){
-        return;
+    int f(int i,int j,int desi,int desj,int &non_obstacle,vector<vector<int>>& grid, vector<vector<int>>&vis,int r,int c,int obs){
+        
+       // if(i<0 || j<0 || i>=r || j>=c || grid[i][j]==-1 || vis[i][j]==1)return 0;
+        if(i==desi && j==desj ){
+            if(obs==non_obstacle)return 1;
+            else return 0;
+        }
+        int ans =0;
+        if(i+1<r && grid[i+1][j]!=-1 &&vis[i+1][j]==0 ){
+            vis[i+1][j]=1;
+           ans+= f(i+1,j,desi,desj,non_obstacle,grid,vis,r,c,obs+1);
+           vis[i+1][j]=0;
+
+        }
+         if(j+1<c && grid[i][j+1]!=-1 && vis[i][j+1]==0 ){
+            vis[i][j+1]=1;
+           ans+= f(i,j+1,desi,desj,non_obstacle,grid,vis,r,c,obs+1);
+           vis[i][j+1]=0;
+
+        }
+         if(i-1>=0 && grid[i-1][j]!=-1 && vis[i-1][j]==0 ){
+            vis[i-1][j]=1;
+           ans+= f(i-1,j,desi,desj,non_obstacle,grid,vis,r,c,obs+1);
+           vis[i-1][j]=0;
+
+        }
+         if(j-1>=0 && grid[i][j-1]!=-1 &&vis[i][j-1]==0){
+            vis[i][j-1]=1;
+           ans+= f(i,j-1,desi,desj,non_obstacle,grid,vis,r,c,obs+1);
+           vis[i][j-1]=0;
+
+        }
+        return ans;
+        
     }
-    if (grid[i][j]==2){
-        if (noObobstacles==count)result++;
-        return;
-    }
-    //backtraking
-    grid[i][j]=-1;
-for(auto &d:dir){
-    int new_i=d[0]+i;
-    int new_j=j+d[1];
-    f(new_i,new_j,count+1,grid);
-}
-    grid[i][j]=0;
-   }
     int uniquePathsIII(vector<vector<int>>& grid) {
-        m=grid.size();
-        n=grid[0].size();
-        int start_i=0;
-        int start_j=0;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]==0)noObobstacles++;
-                if(grid[i][j]==1){
-                    start_i=i;
-                    start_j=j;
+        int non_obstacle =0;
+        int r = grid.size();
+        int c = grid[0].size();
+        int srci=0;
+        int srcj=0;
+        int desi=0;
+        int desj=0;
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(grid[i][j]!=-1){
+                    non_obstacle++;
+                }
+                 if (grid[i][j]==1){
+                    srci = i;
+                    srcj = j;
+                 }
+                 if(grid[i][j]==2){
+                    desi = i;
+                    desj = j;
                 }
             }
         }
-        noObobstacles++;
-        f(start_i,start_j,0,grid);
-        return result;
+        vector<vector<int>>vis(r,vector<int>(c,0));
+          vis[srci][srcj] = 1;
+       return  f(srci,srcj,desi,desj,non_obstacle,grid,vis,r,c,1);
+        
     }
 };
