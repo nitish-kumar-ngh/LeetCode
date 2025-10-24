1d9dabd09f20c1fa86bb7621abb3adb2d61428b8
Nitish Kumar Singh <154716457+nitish-kumar-ngh@users.noreply.github.com>
2025-10-24 19:47:47 +0530

    Time: 3 ms (64.74%), Space: 13.5 MB (56.65%) - LeetHub

--- 0935-orderly-queue/0935-orderly-queue.cpp
+++ 0935-orderly-queue/0935-orderly-queue.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     string orderlyQueue(string s, int k) {
-        if(k > 1) {
-            sort(begin(s), end(s));
+        if(k>1){
+            sort(s.begin(),s.end());
             return s;
+        } 
+        string result =s;
+        for(int len =1;len<s.size();len++){
+            string ans = s.substr(len)+s.substr(0,len);
+            result=min(result,ans);
         }
-        
-        string result = s;
-        for(int i = 1; i<s.length(); i++) {
-            string temp = s.substr(i) + s.substr(0, i);
-            result = min(result, temp);
-        }
-        
         return result;
     }
 };
