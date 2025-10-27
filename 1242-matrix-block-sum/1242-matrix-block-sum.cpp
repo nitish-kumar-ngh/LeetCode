79fc87c25f5d36554c4ab7512acaec8637ecd401
Nitish Kumar Singh <154716457+nitish-kumar-ngh@users.noreply.github.com>
2025-10-27 13:03:30 +0530

    Time: 0 ms (100%), Space: 13.9 MB (17.06%) - LeetHub

--- 1242-matrix-block-sum/1242-matrix-block-sum.cpp
+++ 1242-matrix-block-sum/1242-matrix-block-sum.cpp
@@ -2,29 +2,36 @@ class Solution {
 public:
     vector<vector<int>> matrixBlockSum(vector<vector<int>>& mat, int k) {
         int r = mat.size();
-        int c = mat[0].size();
-     vector<vector<int>>ans(r,vector<int>(c,0));
+        int c=  mat[0].size();
+        vector<vector<int>>prefix(r,vector<int>(c,0));
+        vector<vector<int>>ans(r,vector<int>(c,0));
+        prefix[0][0]=mat[0][0];
+        for(int i=1;i<r;i++)prefix[i][0] = prefix[i-1][0]+mat[i][0];
+        for(int i=1;i<c;i++)prefix[0][i] = prefix[0][i-1]+mat[0][i];
+        for(int i=1;i<r;i++){
+            for(int j =1;j<c;j++){
+                prefix[i][j] = mat[i][j] +prefix[i-1][j]+prefix[i][j-1]-prefix[i-1][j-1];
+            }
+        }
+
         for(int i=0;i<r;i++){
             for(int j =0;j<c;j++){
-                int x=0;
-                int nr = i-k;
-                if(nr<0)nr =0;
-                int nc=j-k;
-                if(nc<0)nc =0;
-                int NR= i+k;
-                if(NR>=r) NR=r-1;
-                int NC= j+k;
-                if(NC>=c)NC=c-1;
-                for(int k=nr;k<=NR;k++){
-                   for(int l=nc;l<=NC;l++){
-                       x+=mat[k][l];
-                   }
-                }
-                ans[i][j] = x;
-                
+             //   prefix[i][j] = mat[i][j] +prefix[i-1][j]+prefix[i][j-1]-prefix[i-1][j-1];
+          int r1 = max(0, i - k);
+int c1 = max(0, j - k);
+
+             int r2 = min(r-1,i+k);
+             int c2 = min(c-1,j+k);
+             
+             
+             
+          ans[i][j] = prefix[r2][c2]
+           - (c1 > 0 ? prefix[r2][c1 - 1] : 0)
+           - (r1 > 0 ? prefix[r1 - 1][c2] : 0)
+           + (r1 > 0 && c1 > 0 ? prefix[r1 - 1][c1 - 1] : 0);
+
             }
         }
         return ans;
-
     }
 };
