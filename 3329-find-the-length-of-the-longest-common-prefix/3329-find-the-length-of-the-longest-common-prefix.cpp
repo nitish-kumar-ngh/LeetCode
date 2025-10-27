9d977c611ada86cff3c5d9b017ecba7b1644afde
Nitish Kumar Singh <154716457+nitish-kumar-ngh@users.noreply.github.com>
2025-10-27 18:41:39 +0530

    Time: 129 ms (87.92%), Space: 160.7 MB (49.37%) - LeetHub

--- 3329-find-the-length-of-the-longest-common-prefix/3329-find-the-length-of-the-longest-common-prefix.cpp
+++ 3329-find-the-length-of-the-longest-common-prefix/3329-find-the-length-of-the-longest-common-prefix.cpp
@@ -1,25 +1,53 @@
 class Solution {
 public:
-    int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
-        unordered_set<int>s;
-        for(int i =0;i<arr1.size();i++){
-            int num = arr1[i];
-            while(num>0){
-                s.insert(num);
-                num=num/10;
+    class TrieNode{
+        public:
+       TrieNode*character[10];
+    };
+
+    TrieNode* getnode(){
+          TrieNode*temp = new TrieNode();
+          for(int i =0;i<10;i++){
+            temp->character[i]=NULL;
+          }
+          return temp;
+    }
+    void insert(int num,TrieNode*root){
+        TrieNode* temp = root;
+        string numstr = to_string(num);
+        for(int  i =0;i<numstr.size();i++){
+            int idx = numstr[i]-'0';
+            if(temp->character[idx]==NULL){
+                temp->character[idx] = getnode();
             }
+            temp =  temp->character[idx] ;
+        }
+        
+    }
+    int  search(int num, TrieNode* root){
+        string str = to_string(num);
+        TrieNode* temp = root;
+        int len =0;
+        for(int i =0;i<str.size();i++){
+            int idx = str[i]-'0';
+            if(temp->character[idx]!=NULL){
+                len++;
+                temp=temp->character[idx];
+            }else{
+                break;
+            }
+        }
+        return len;
+    }
+    int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
+        TrieNode* root = getnode();
+        for(int i =0;i<arr1.size();i++){
+            insert(arr1[i],root);
         }
         int result =0;
-
         for(int i =0;i<arr2.size();i++){
-            int num = arr2[i];
-            while(num>0 && s.find(num)==s.end()){
-                num = num/10;
-            }
-
-           if(num>0) result = max(result,static_cast<int>(log10(num)+1));
+            result = max(result , search(arr2[i],root));
         }
         return result;
-
     }
 };
